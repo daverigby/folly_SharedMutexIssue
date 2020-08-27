8844961c0858dfc2d0aeb5edc561851b6a92b0ff
Dave Rigby <daver@couchbase.com>
2020-08-27 22:19:33 +0100

    Simplification of testcase

--- main.cc
+++ main.cc
@@ -1,61 +1,11 @@
-#include "shared.h"
+#include <folly/ThreadLocal.h>
 
-#include <folly/synchronization/Baton.h>
-#include <iostream>
-
-//folly::ThreadLocal<int, TestTag> localInt;
-
-std::thread makeThread(Obj& object, folly::Baton<>& shutdown) {
-    folly::Baton<> baton;
-    std::thread thread([&object, &baton, &shutdown] {
-        *object.localInt = 1;
-        baton.post();
-        // Keep the thread-local around until shutdown.
-        shutdown.wait();
-    });
-    // Ensure thread has set the thread-local before returning.
-    baton.wait();
-    return std::move(thread);
-}
-
+FOLLY_EXPORT void setThreadLocalViaShared(folly::ThreadLocal<int>&);
 
 int main() {
-    Obj object;
+    // Define one thread-local from the main module.
+    folly::ThreadLocal<int> localInt1;
 
-    folly::Baton<> shutdown;
-
-    // Set thread-local of 'object' using a thread created via a dll.
-//    auto thread1 = makeThreadViaShared(shutdown);
-    auto thread1 = makeThread(object, shutdown);
-
-#if 1
-    // then via a thread created here.
-    folly::Baton<> localSet;
-    auto thread2 = std::thread([&object, &localSet, &shutdown] {
-        *object.localInt = 2;
-        localSet.post();
-        shutdown.wait();
-    });
-
-
-    // Wait for local to be set everywhere, then sum up values. Should
-    // be 1 + 2.
-    localSet.wait();
-#endif
-
-    *object.localInt = 2;
-
-    int total = 0;
-    for (const auto& i : object.localInt.accessAllThreads()) {
-        total += i;
-    }
-    std::cerr << "total:" << total << "\n";
-
-    if (total != 1 + 2) {
-        std::abort();
-    }
-
-    shutdown.post();
-    thread1.join();
-    thread2.join();
+    // Also set the same thread-local `localInt` via a dll.
+    setThreadLocalViaShared(localInt1);
 }
--- shared.cc
+++ shared.cc
@@ -1,17 +1,5 @@
-#include "shared.h"
-
-#include <functional>
 #include <folly/ThreadLocal.h>
 
-std::thread makeThreadViaShared(folly::Baton<>& shutdown) {
-    folly::Baton<> baton;
-    std::thread thread([&baton, &shutdown] {
-//        *localInt = 1;
-        baton.post();
-        // Keep the thread-local around until shutdown.
-        shutdown.wait();
-    });
-    // Ensure thread has set the thread-local before returning.
-    baton.wait();
-    return std::move(thread);
+FOLLY_EXPORT void setThreadLocalViaShared(folly::ThreadLocal<int>& localInt) {
+        *localInt = 1;
 }
