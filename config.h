ef38a686730efaa5c964e52ee8e1164639ddd8ee
Dave Rigby <daver@couchbase.com>
2021-04-12 20:27:25 +0100

    Change default Mutex to broken SharedMutex without tag
    To demonstrate the issue.

--- config.h
+++ config.h
@@ -5,6 +5,9 @@
 // Adding my own tag which has external (default) visibility, ensuring
 // SharedMutexImpl<false, MyTag, ...>::deferredReaders has weak
 // linkage) runs successfully.
+#if defined(EXPLICIT_TAG)
 struct FOLLY_EXPORT MyTag {};
-
 using Mutex = folly::SharedMutexImpl<false, MyTag>;
+#else
+using Mutex = folly::SharedMutex;;
+#endif
